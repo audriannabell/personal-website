4e30c59637c4451413859119f0e7a17ffb9b0bd5
audriannabell <75336859+audriannabell@users.noreply.github.com>
2022-02-05 20:55:48 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 //nav buttons
 
-let projectButton = document.querySelector('.projects__nav') 
-let contactButton = document.querySelector('.contact__nav') 
-let resumeButton = document.querySelector('.resume__nav') 
-let aboutButton = document.querySelector('.about__nav') 
+let projectButton = document.querySelector('.projects__nav') ;
+let projectButton2 = document.querySelector('.projects__nav2');
+let contactButton = document.querySelector('.contact__nav') ;
+let contactButton2 = document.querySelector('.contact__nav2')
+let aboutButton = document.querySelector('.about__nav') ;
+let skillsButton = document.querySelector('.skills__nav') ;
+let resumeButton = document.querySelector('.resume__nav');
+let homeButton = document.querySelector('.home__nav');
 
-projectButton.addEventListener("click", function (e){
+homeButton.addEventListener("click", function (e){
     e.preventDefault();
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
     behavior: 'smooth',
     block: 'start',
+    inline: 'center'
+
+});
+});
+
+aboutButton.addEventListener("click", function (e){
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'center'
+    });
+});
+
+projectButton.addEventListener("click", function (e){
+   e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'center'
+
+});
+});
+
+projectButton2.addEventListener("click", function (e){
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'center'
+
+});
+});
+
+skillsButton.addEventListener("click", function (e){
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
     inline: 'center'
 
 });
@@ -22,22 +69,28 @@ contactButton.addEventListener("click", function (e){
     document.querySelector(this.getAttribute('href')).scrollIntoView({
     behavior: 'smooth',
     block: 'nearest',
-    inline: 'center'
-});
+    inline: 'center'});
 });
 
-aboutButton.addEventListener("click", function (e){
+contactButton2.addEventListener("click", function (e){
     e.preventDefault();
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
     behavior: 'smooth',
-    block: 'start',
-    inline: 'nearest'
+    block: 'nearest',
+    inline: 'center'});
+});
+
+resumeButton.addEventListener("click", function (e){
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'center'
+
 });
 });
 
-//when navbar is sticky, toggle to background - dred
-
-
-
+//section in viewport is underlined
 
